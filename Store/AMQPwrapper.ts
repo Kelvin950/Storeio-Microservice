7158101c12cbd7887bf3468e47ae53e4e3f32378
kelvin <kelvin950>
2023-03-02 16:25:53 +0000

    creating an amqp wrapper

---
+++ Store/AMQPwrapper.ts
@@ -0,0 +1,41 @@
+import {Channel , Connection ,connect} from 'amqplib'
+
+class AMQP{
+
+
+ 
+ private _client?:Connection ; 
+ private _channel?:Channel ;
+
+get client(){
+
+    if(!this._client){
+        throw new Error("Connection to amqp failed");
+    }
+
+    return this._client
+}
+
+get channel(){
+
+  if (!this._channel) {
+    throw new Error("Connection to amqp failed");
+  }
+
+  return this._channel;
+}
+
+ 
+  async connect(url:string){
+     
+    this._client = await connect(url); 
+     
+    this._channel =  await this._client.createChannel() ;
+
+
+   }
+
+ }
+
+
+ export const AmqplibWrapper = new AMQP(); 
