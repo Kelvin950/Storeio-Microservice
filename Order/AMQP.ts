7c86f1c3689584618548f794b409f7dd0e3be9b5
kelvin <denlinato@gmail.com>
2023-07-10 21:35:08 +0000

    adding config to order service

---
+++ Order/AMQP.ts
@@ -0,0 +1,41 @@
+import {Connection , Channel, connect} from 'amqplib'
+
+
+class AMQPLIBWRAPPER {
+    
+
+    private _channel?:Channel ;
+    private _client?:Connection ;
+
+   get client(){
+
+ 
+
+    if(!this._client)throw new Error('cannot connect to client');
+
+
+    return this._client ;
+
+
+    }
+
+
+    async Connect(url:string){
+
+      try {
+        
+        this._client =  await  connect(url) ;
+            this._channel =  await  this._client.createChannel() ;
+      } catch (error) {
+        
+        throw error ;
+      }      
+      
+
+
+    }
+}
+
+
+
+export const AMQPWRAPPER =  new AMQPLIBWRAPPER()
--- Order/index.ts
+++ Order/index.ts
@@ -1,6 +1,57 @@
 import {app} from './src/app' ;
+import  {AMQPWRAPPER } from './AMQP' ;
+const PORT=  process.env.PORT || 3000;
 
 
+
+(async()=>{
+
+ 
+
+
+    try {
+        
+       if(!process.env.SECRET) throw new Error('no cookie secret');
+
+       if(!process.env.Cassandra_URI) throw new Error('no mongo uri');
+
+       if(!process.env.AMQP_URI) throw new Error('no amqp uri') ;
+
+
+await AMQPWRAPPER.Connect(process.env.AMQP_URI) ;
+
+
+process.on('SIGINT' , async()=>{
+
+    await  AMQPWRAPPER.client.close() ;
+})
+
+
+      
+
+process.on("SIGTERM", async () => {
+  await AMQPWRAPPER.client.close();
+});
+
+
+
+
+       
+
+
+        app.listen(PORT, ()=>{
+
+            console.log("Server opened") ;
+        })
+
+
+    } catch (error) {
+        
+
+        throw new Error('connection failed')
+    }
+
+})()
 
 
 
